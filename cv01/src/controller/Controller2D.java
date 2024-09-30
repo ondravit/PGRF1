02b7d879dbbe36ffcefc556510db9da66872019d
ondravit <ondra.vit04@seznam.cz>
2024-09-30 15:55:40 +0200

    uprava metod pro kresleni mysi, implementace kresleni klavesy

--- cv01/src/controller/Controller2D.java
+++ cv01/src/controller/Controller2D.java
@@ -2,12 +2,12 @@ package controller;
 
 import view.Panel;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Controller2D {
     private final Panel panel;
+    private int x, y;
 
     public Controller2D(Panel panel) {
         this.panel = panel;
@@ -31,12 +31,21 @@ public class Controller2D {
         panel.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                if (e.getButton() == MouseEvent.BUTTON1)
-                    panel.getRaster().setRGB(e.getX(), e.getY(), 0xffff00);
-                if (e.getButton() == MouseEvent.BUTTON2)
-                    panel.getRaster().setRGB(e.getX(), e.getY(), 0xff00ff);
-                if (e.getButton() == MouseEvent.BUTTON3)
-                    panel.getRaster().setRGB(e.getX(), e.getY(), 0xffffff);
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    x = e.getX();
+                    y = e.getY();
+                    draw();
+                }
+                if (e.getButton() == MouseEvent.BUTTON2) {
+                    x = e.getX();
+                    y = e.getY();
+                    draw();
+                }
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    x = e.getX();
+                    y = e.getY();
+                    draw();
+                }
                 panel.repaint();
             }
         });
@@ -44,14 +53,62 @@ public class Controller2D {
         panel.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
-                if (e.getButton() == MouseEvent.BUTTON1)
-                    panel.getRaster().setRGB(e.getX(), e.getY(), 0xffff00);
-                if (e.getButton() == MouseEvent.BUTTON2)
-                    panel.getRaster().setRGB(e.getX(), e.getY(), 0xff00ff);
-                if (e.getButton() == MouseEvent.BUTTON3)
-                    panel.getRaster().setRGB(e.getX(), e.getY(), 0xffffff);
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    x = e.getX();
+                    y = e.getY();
+                    draw();
+                }
+                if (e.getButton() == MouseEvent.BUTTON2) {
+                    x = e.getX();
+                    y = e.getY();
+                    draw();
+                }
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    x = e.getX();
+                    y = e.getY();
+                    draw();
+                }
                 panel.repaint();
             }
         });
+
+        panel.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        x--;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        x++;
+                        break;
+                    case KeyEvent.VK_UP:
+                        y--;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        y++;
+                        break;
+                }
+                draw();
+                panel.repaint();
+            }
+        });
+    }
+
+    private void draw() {
+        clear();
+        panel.getRaster().setRGB(x, y, 0xff0000);
+    }
+
+    public void start() {
+        x = panel.getWidth()/2;
+        y = panel.getHeight()/2;
+        draw();
+        panel.repaint();
+    }
+    public void clear() {
+        Graphics g = panel.getGraphics();
+        g.setColor(Color.black);
+        g.fillRect(0, 0, panel.getWidth(), panel.getHeight());
     }
 }
